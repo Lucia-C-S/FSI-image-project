2e21337db3f30fe22fa48363f28c2fbfc51b9ec8
Lucia Castrillo Sanchez <luciacs.lu@gmail.com>
2026-05-01 23:17:50 +0200

    Actualizando task 6

--- task6RecognizeCharacters.m
+++ task6RecognizeCharacters.m
@@ -1,54 +1,54 @@
-function [letter, score] = task6RecognizeCharacters(img, alphabetDir)
+function [letter, score] = task6RecognizeCharacters(img, alphabet)
     % INPUT PARAMETERS:
     %   -img: segmented character (from Task 3)
-    %   -alphabetDir: folder containing templates (A.png, B.png, …)
+    %   -alphabet: struct of alphabet
     % OUTPUT PARAMETERS:
     %   - letter: predicted character
     %   - score: similarity score
-    
-    % charNormCell = task4PreprocessingCharacters({img}, 32); % apply task4 preprocessing to cell array of img
-    % %  to convert each image (character) into fixed size 32x32 and double precision [0,1]
-    % charNorm = im2double(charNormCell{1}); %extract unique index
 
     % --- Step 1: normalize single character ---
     img = im2double(img);
+    % [h, w] = size(img);
+    % % PADDING
+    % % we convert any character image into a square image before resizing, 
+    % % because distortion happens if we resize non-square images directly
+    % if h > w % case image is taller than wide
+    %     dif = h - w; %missing width
+    %     left  = floor(dif/2);
+    %     right = ceil(dif/2); %bc if d is odd we would not split equally
+    %     img = padarray(img, [0 left], 0, 'pre'); %add rows on left
+    %     img = padarray(img, [0 right], 0, 'post');%add rows on right
+    % 
+    % elseif w > h %width of image>height
+    %     dif = w - h;
+    %     top    = floor(dif/2);
+    %     bottom = ceil(dif/2);
+    %     img = padarray(img, [top 0], 0, 'pre'); %add rows on top
+    %     img = padarray(img, [bottom 0], 0, 'post'); % bottom
+    % end
+    % 
+    % charNorm = imresize(img, [32 32]);
 
-    [h, w] = size(img);
+    charNormCell = task4PreprocessingCharacters({img}, 32);
+    charNorm = im2double(charNormCell{1});
 
-    if h > w
-        dif = h - w;
-        left  = floor(dif/2);
-        right = ceil(dif/2);
-        img = padarray(img, [0 left], 0, 'pre');
-        img = padarray(img, [0 right], 0, 'post');
-
-    elseif w > h
-        dif = w - h;
-        top    = floor(dif/2);
-        bottom = ceil(dif/2);
-        img = padarray(img, [top 0], 0, 'pre');
-        img = padarray(img, [bottom 0], 0, 'post');
-    end
-
-    charNorm = imresize(img, [32 32]);
-
-
-    letters = fieldnames(alphabetDir);
-% 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; %define search space
-    scores  = zeros(1, 26); %preallocate for speed
+    letters = fieldnames(alphabet);
+% 'ABCDEFGHIJKLMNOPQRSTUVWXYZ': define search space
+    n = length(letters);
+    scores = -inf(1, n); %preallocate for speed
     
-    for k = 1:26 %iterate over each possible letter
-        ref = alphabetDir.(letters{k});  % Get reference template from struct
-        ref = im2double(ref);
-        ref = imresize(ref, [32 32]);   % ensure same size
-
+    for k = 1:n %iterate over each possible letter
          %skip missing templates (score remains 0)
-          % --- Skip missing or empty templates ---
-        if ~isfield(alphabetDir, letters{k}) || isempty(alphabetDir.(letters{k}))
+        if isempty(alphabet.(letters{k}))
             continue;
         end
+        ref = im2double(alphabet.(letters{k}));  % Get reference template from struct
+        ref = imresize(ref, [32 32]);   % ensure same size
         
-        % normalize (zero mean, unit variance) into -1,1
+        % normalize (zero mean, unit variance) into -1,1 bc corr2 is:
+        % -Output range: [-1, 1]
+        % Scale invariant
+        % Independent of image size
         scores(k) = corr2(charNorm, ref);
     end
     
@@ -57,7 +57,7 @@ function [letter, score] = task6RecognizeCharacters(img, alphabetDir)
     best = sortedScores(1); %select best match
     second = sortedScores(2);
 
-    letter = letters(idxs(1)); %map from index to letter
+    letter = letters{idxs(1)}; %map from index to letter
     score  = best;
 
     % Confidence check
